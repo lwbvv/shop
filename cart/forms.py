4d010b1233288a2666bb3db1105d89016fdb1a4f
lwbvv <lwbvv@naver.com>
2020-03-27 11:54:29 +0000

    Feat cart view.py (add def: add)

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class AddProductForm(forms.Form):
+     # 상품 수량 설정 field
     quantity = forms.IntegerField()
+     # 수정 여부 확인 field
     is_update = forms.BooleanField(required = False, initial=False, widget=forms.HiddenInput)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
+from shop.models import Product
+from .forms import AddProductForm
+from .cart import Cart
+
+@require_POST
+def add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+
+    form = AddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+
+        return redirect('cart:detail')
 
 # Create your views here.
